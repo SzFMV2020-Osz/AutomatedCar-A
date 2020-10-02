a1b773c9eea2872623ac2074ff4f8f03bb5f9b66
erikferenci <ferenci.erik00@gmail.com>
2020-10-02 20:03:26 +0200

    Added basic classes: Powertrain and Motor

---
+++ src/AutomatedCar/Models/Motor.cs
@@ -0,0 +1,35 @@
+﻿namespace AutomatedCar.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Motor
+    {
+        private int rpm;
+        private double gasPedal;
+        private double breakPedal;
+        private Gears gearPosition;
+        private int automaticShiftGear;
+
+        public int Rpm { get => this.rpm; set => this.rpm = this.CheckRpm(); }
+
+        public Gears GearPosition { get => this.gearPosition; set => this.gearPosition = value; }
+
+        public int AutomaticShiftGear { get => this.automaticShiftGear; set => this.automaticShiftGear = value; }
+
+        public int GasPedalToSpeed()
+        {
+            return 0;
+        }
+
+        private int CheckRpm()
+        {
+            return 0;
+        }
+
+        private void BreakPedalAction()
+        {
+        }
+    }
+}
---
+++ src/AutomatedCar/Models/Powertrain.cs
@@ -0,0 +1,36 @@
+﻿namespace AutomatedCar.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Powertrain
+    {
+        private double speed;
+        private double steering;
+        private Motor motor;
+
+        public Powertrain()
+        {
+            this.motor = new Motor();
+        }
+
+        public double Speed { get => this.speed; set => this.speed = this.motor.GasPedalToSpeed(); }
+
+        private double Steering { get => this.steering; set => this.steering = this.SteeringWheel(); }
+
+        public void VectorCalculator()
+        {
+            // use: motor.GearPosition, this.Speed, this.Steering
+        }
+
+        private void EvaluateInput()
+        {
+        }
+
+        private double SteeringWheel()
+        {
+            return 0;
+        }
+    }
+}
