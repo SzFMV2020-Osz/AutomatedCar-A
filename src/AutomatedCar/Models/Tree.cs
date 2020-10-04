718208c4ea56b875a25b6fe625c4fd49fcd1f906
ZSOLTI-PC\Zsolti <bozsik.zsolt97@gmail.com>
2020-10-04 20:08:58 +0200

    Create tree class

---
+++ src/AutomatedCar/Models/Tree.cs
@@ -0,0 +1,15 @@
+namespace AutomatedCar.Models
+{
+    using Avalonia.Controls.Shapes;
+
+    public class Tree : StaticGameItems
+    {
+        public Tree(int x, int y, string filename, RotationMatrix rotmatrix, Polygon wood) : base(x, y, filename, rotmatrix)
+        {
+            this.Wood = wood;
+            this.ZIndex = 1;
+        }
+
+        private Polygon Wood { get; set; }
+    }
+}
