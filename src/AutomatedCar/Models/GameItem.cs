7ab021afe0f08be06b2c86805d6917c4919a1e6d
dumbi <davidmeszaros996@gmail.com>
2020-10-07 19:35:00 +0200

    Fix for jsonParser

--- src/AutomatedCar/Models/GameItem.cs
+++ src/AutomatedCar/Models/GameItem.cs
@@ -1,12 +1,19 @@
+using Avalonia.Controls.Shapes;
+using System.Collections.Generic;
+
 namespace AutomatedCar.Models
 {
     public class GameItem : WorldObject
     {
-        public GameItem(int x, int y, string filename, bool iscolliding) : base(x, y, filename)
+        public GameItem(int x, int y, string filename, bool iscolliding)
+            : base(x, y, filename)
         {
             this.IsColliding = iscolliding;
+            this.Polygons = new List<Polygon>();
         }
-        
+
         public bool IsColliding { get; set; }
+
+        public List<Polygon> Polygons { get; set; }
     }
 }
